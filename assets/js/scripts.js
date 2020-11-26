bea7399a6ec67fe3f26f8184d105d87270aad2b3
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2020-11-26 13:53:09 +0800

    corrected syntax error on js file.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,7 +9,9 @@ btncreateEl.addEventListener("click", function () {
 let charLength = prompt("Select the length of your desired password - must be value between 8 and 128.");
 
 
-// PseudoCode
+})
+
+/* PseudoCode
 // Need to create var setting the Elements that will be used to create NewPassword and that will be used to Start the prompts for the EventListener.
 // Create an EventListener based on the clicking of the button that will then initiate a serious of promtps.
 // The First Prompt will be used to to obtain the required character length for the password. This must be between 8 - 128 characters.
@@ -21,4 +23,4 @@ let charLength = prompt("Select the length of your desired password - must be va
 // Create if statments that loops for every combination of passwords characters using random.math and limit to character length as per var from first prompt and made up from relevant variable or variable combo.
 // Create if statememts that loops for single character sets selected ie symbols || numbers || lowercase || uppercase    
 // Create if statememts that loops for Combo Character options selected ie combo 1 || combo 2 || combo 3 etc.
-// The results from the function will then be used to create the newPassword to be displayed in the textArea
+// The results from the function will then be used to create the newPassword to be displayed in the textArea */
