f67c4dfe08490922edce5c3dac8b867884e5e94a
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2020-11-26 13:11:18 +0800

    created a pseudocode for my password generator.

---
+++ assets/js/scripts.js
@@ -0,0 +1,14 @@
+
+// PseudoCode
+// Need to create var setting the Elements that will be used to create NewPassword and that will be used to Start the prompts for the EventListener.
+// Create an EventListener based on the clicking of the button that will then initiate a serious of promtps.
+// The First Prompt will be used to to obtain the required character length for the password. This must be between 8 - 128 characters.
+// If the Character length specified is within specified parameters then run the function to prompt the user on what types of characters are to be included.
+// Set the variables that will be available for the Password Generator to be inlcued in the TextArea 
+// Set a blank string for the new password to be generated into that will then become the content of the #password textarea.
+// Create variables that embody all the possible different combinations 
+// Call out 4 Prompts so that the user can confirm what character types they wish their password to include
+// Create if statments that loops for every combination of passwords characters using random.math and limit to character length as per var from first prompt and made up from relevant variable or variable combo.
+// Create if statememts that loops for single character sets selected ie symbols || numbers || lowercase || uppercase    
+// Create if statememts that loops for Combo Character options selected ie combo 1 || combo 2 || combo 3 etc.
+// The results from the function will then be used to create the newPassword to be displayed in the textArea
